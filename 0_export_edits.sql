224bef2e408d2f13602fd8427b2f32826a0c72ec
Luggen Michael <michael.luggen@bfh.ch>
2019-07-04 19:44:12 +0000

    clean-up

--- 0_export_edits.sql
+++ 0_export_edits.sql
@@ -1,1 +1,13 @@
-select page_title, rev_comment, rev_user_text, rev_timestamp from revisions where rev_comment like '%[[Property:%]]%[[Q%'  order by rev_id INTO OUTFILE 'edits.csv';
+    SELECT
+        page_title,
+        rev_comment,
+        rev_user_text,
+        rev_timestamp 
+    FROM
+        revisions 
+    WHERE
+        rev_comment LIKE '%[[Property:%]]%[[Q%'
+    ORDER BY
+        rev_id 
+    INTO
+        OUTFILE 'edits_wikidatawiki-20181001-pages.csv';
